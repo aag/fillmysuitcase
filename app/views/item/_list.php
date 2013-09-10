e7ebaa88abe29383185d2fbc483b78ae2ec7f598
Adam Goforth <code@adamgoforth.com>
2013-09-10 20:37:33 +0200

    Update the style of the list page

--- app/views/item/_list.php
+++ app/views/item/_list.php
@@ -1,11 +1,16 @@
 <div id="list" ng-controller="ListCtrl">
     <div class="unpacked">
+        <h3>Not Packed</h3>
         <ul class="packing-list">
             <li ng-repeat="item in items | filter:{packed:false}">
                 <form class="form-inline">
-                    <input type="checkbox" class="check" ng-model="item.packed" ng-change="change(item)">
-                    <input ng-model="item.name" ng-change="change(item)">
-                    <a href="" ng-click="delete(item)" ng-hide="item.confirmingDelete"><img src="/img/cross.png"></a>
+                    <div class="check-holder">
+                        <input type="checkbox" class="check" ng-model="item.packed" ng-change="change(item)">
+                    </div>
+                    <input class="item-name" ng-model="item.name" ng-change="change(item)">
+                    <a href="" ng-click="delete(item)" ng-hide="item.confirmingDelete">
+                        <img class="delete-icon" alt="Delete item" title="Delete item" src="/img/cross.png">
+                    </a>
                     <div class="confirm-delete" ng-show="item.confirmingDelete">
                         Are you sure?
                         <button class="delete-yes" ng-click="confirmDelete(item)">Delete</button>
@@ -15,7 +20,8 @@
             </li>
             <li class="new-item">
                 <form class="form-inline" ng-submit="submit()">
-                    <input placeholder="New Item" ng-model="newName">
+                    <div class="check-holder"></div>
+                    <input placeholder="New Item" class="item-name" ng-model="newName">
                     <button type="submit">Create</button>
                 </form>
             </li>
@@ -28,7 +34,7 @@
             <li ng-repeat="item in items | filter:{packed:true}">
                 <form class="form-inline">
                     <input type="checkbox" class="check" ng-model="item.packed" ng-change="change(item)">
-                    <input ng-model="item.name" ng-change="change(item)">
+                    <div class="checked-item">{{ item.name }}</div>
                 </form>
             </li>
         </ul>
