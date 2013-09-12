3703ff13e000efd1f820f735ffd045fe1ee3d1f9
Adam Goforth <code@adamgoforth.com>
2013-09-12 12:37:49 +0200

    Add some verbiage to the root page

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -2,10 +2,25 @@
 
 @section('content')
     <div class="jumbotron">
-        <h1>Remember to pack everything</h1>
+        <h1>Pack exactly what you need</h1>
+    </div>
+    <div>
+        <p>
+            Have you ever gone on a trip and forgotten something important? Maybe
+            you have the opposite problem and pack too much. The problem is that
+            every time you pack for a trip, you have to start over from the
+            beginning and remember what to bring. It's hard to remember what
+            you packed last time, so you pack too little or too much.
+        </p>
+        <p>
+            Fill My Suitcase is a packing list that you use every time you pack.
+            If you forget something on this trip, add it to your list and you'll
+            never forget it again. Did you take something and not use it? Take
+            it off of your list and never pack it again.
+        </p>
     </div>
     <div class="list-link">
-        <h3><a href="{{ URL::route('listpage') }}">Go to my packing list</a></h3>
+        <h3><a href="{{ URL::route('listpage') }}">Go to your packing list</a></h3>
     </div>
 @stop
 
