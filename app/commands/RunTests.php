dfb1e681ecbc1c7b2ae0bf48f7998fec303254aa
Adam Goforth <code@adamgoforth.com>
2014-12-02 07:36:32 +0100

    Add "artisan test" command

---
+++ app/commands/RunTests.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * A Command that runs all of the available tests and outputs
+ * the results to the console.
+ */
+
+use Illuminate\Console\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+
+class RunTests extends Command {
+
+	/**
+	 * The console command name.
+	 *
+	 * @var string
+	 */
+	protected $name = 'test';
+
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Run all available tests.';
+
+	/**
+	 * Create a new command instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	/**
+	 * Execute the console command.
+	 *
+	 * @return mixed
+	 */
+	public function fire()
+	{
+		passthru('vendor/bin/phpunit');
+	}
+
+	/**
+	 * Get the console command arguments.
+	 *
+	 * @return array
+	 */
+	protected function getArguments()
+	{
+		return [];
+	}
+
+	/**
+	 * Get the console command options.
+	 *
+	 * @return array
+	 */
+	protected function getOptions()
+	{
+		return [];
+	}
+
+}
