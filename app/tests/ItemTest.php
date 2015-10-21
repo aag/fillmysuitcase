353dc2d564fe340029f92756918c55dacaa684e7
Adam Goforth <code@adamgoforth.com>
2015-10-21 10:18:53 +0200

    Refactor Item tests and add default packed test

--- app/tests/ItemTest.php
+++ app/tests/ItemTest.php
@@ -2,58 +2,69 @@
 
 class ItemTest extends TestCase {
 
-    public function setUp()
+    public function testValidItemCreation()
     {
-        parent::setUp();
-
-        $this->validAttribs = array(
+        $validAttribs = array(
             'name'   => 'Socks',
             'packed' => false,
         );
-        $this->validItem = new Item($this->validAttribs);
-    }
+        $validItem = new Item($validAttribs);
 
-    public function tearDown()
-    {
-    }
-
-    public function testValidItemCreation()
-    {
-        $this->assertEquals($this->validAttribs['name'], $this->validItem->name);
-        $this->assertEquals($this->validAttribs['packed'], $this->validItem->packed);
+        $this->assertEquals('Socks', $validItem->name);
     }
 
     public function testValidItemValidation()
     {
-        $this->assertTrue($this->validItem->validate());
+        $validAttribs = array(
+            'name'   => 'Socks',
+            'packed' => false,
+        );
+        $validItem = new Item($validAttribs);
+
+        $this->assertTrue($validItem->validate());
     }
 
     public function testEmptyNameIsInvalid()
     {
-        $attrs = $this->validAttribs;
-        unset($attrs['name']);
+        $item = new Item([
+            'packed' => false
+        ]);
 
-        $item = new Item($attrs);
         $this->assertFalse($item->validate());
+    }
+
+    public function testUnsetPackedIsDefaultFalse()
+    {
+        $item = new Item([
+            'name' => 'Shoes'
+        ]);
+
+        $this->assertFalse($item->packed);
+    }
+
+    public function testLongNameIsValid()
+    {
+        // Try with the maximum valid length
+        $longName = '12345678901234567890123456789012345678901234567890' .
+                    '12345678901234567890123456789012345678901234567890';
+
+        $item = new Item([
+            'name' => $longName,
+        ]);
+        $this->assertTrue($item->validate());
     }
 
     public function testLongNameIsInvalid()
     {
-        // First try with the maximum valid length
+        // Try with a name that it just too long
         $longName = '12345678901234567890123456789012345678901234567890' .
                     '12345678901234567890123456789012345678901234567890';
 
-        $attrs = $this->validAttribs;
-        $attrs['name'] = $longName;
-
-        $item = new Item($attrs);
-        $this->assertTrue($item->validate());
-
-        // Then try with a name that it just too long
         $tooLongName = $longName . '!';
-        $attrs['name'] = $tooLongName;
+        $item = new Item([
+            'name' => $tooLongName,
+        ]);
 
-        $item = new Item($attrs);
         $this->assertFalse($item->validate());
     }
 
