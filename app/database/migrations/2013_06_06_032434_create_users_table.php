5f06902437558c2006dba387fcd51d53937d1f42
Adam Goforth <code@adamgoforth.com>
2015-10-25 15:32:53 +0100

    Convert tabs to spaces in DB migration files

--- app/database/migrations/2013_06_06_032434_create_users_table.php
+++ app/database/migrations/2013_06_06_032434_create_users_table.php
@@ -5,32 +5,32 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateUsersTable extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('users', function(Blueprint $table)
-		{
-			$table->increments('id');
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function(Blueprint $table)
+        {
+            $table->increments('id');
             $table->string('username', 64);
             $table->string('password', 255);
             $table->string('email', 255);
             $table->string('remember_token', 100)->nullable();
-			$table->timestamps();
-		});
-	}
+            $table->timestamps();
+        });
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('users');
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
 
 }
--- app/database/migrations/2013_06_23_043832_create_items_table.php
+++ app/database/migrations/2013_06_23_043832_create_items_table.php
@@ -5,36 +5,36 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateItemsTable extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('items', function(Blueprint $table)
-		{
-			$table->increments('id');
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function(Blueprint $table)
+        {
+            $table->increments('id');
             $table->integer('user_id')->unsigned();
             $table->string('name');
             $table->boolean('packed')->default(false);
-			$table->timestamps();
+            $table->timestamps();
 
             $table->foreign('user_id')
                     ->references('id')
                     ->on('users')
                     ->onDelete('cascade');
-		});
-	}
+        });
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('items');
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('items');
+    }
 
 }
