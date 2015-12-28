42993ca52eacc9395d18bf644b561fef48ee27f2
Adam Goforth <code@adamgoforth.com>
2015-12-28 18:03:24 -0500

    Add user validation tests

--- tests/Integration/UserTest.php
+++ tests/Integration/UserTest.php
@@ -7,10 +7,35 @@ use App\Models\Item;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Validator;
 
 class UserTest extends \Tests\TestCase {
 
     use DatabaseMigrations, DatabaseTransactions;
+
+    /**
+     * Takes an array of user property names and values, then returns true
+     * if the properties are valid according to the user validation rules, else
+     * false.
+     * 
+     * @param array $properties 
+     * @access public
+     * @return bool
+     */
+    public function valid(array $properties)
+    {
+        $allUserValidationRules = User::getAllValidationRules();
+
+        $validationRules = [];
+        foreach ($properties as $name => $value) {
+            if (isset($allUserValidationRules[$name])) {
+                $validationRules[$name] = $allUserValidationRules[$name];
+            }
+        }
+
+        $validator = Validator::make($properties, $validationRules);
+        return $validator->passes();
+    }
 
     public function testMaxItemsPerUser()
     {
@@ -44,4 +69,67 @@ class UserTest extends \Tests\TestCase {
         $this->assertFalse($user->hasMaxItems());
     }
 
+    public function testValidUsernameValidation()
+    {
+        $this->assertTrue($this->valid(['username' => 'Test Username']));
+    }
+
+    public function testEmptyUsernameIsInvalid()
+    {
+        $this->assertFalse($this->valid(['username' => '']));
+    }
+
+    public function testEmptyPasswordIsInvalid()
+    {
+        $this->assertFalse($this->valid(['password' => '']));
+    }
+
+    public function testShortPasswordIsInvalid()
+    {
+        $this->assertFalse($this->valid([
+            'password' => 'abcde',
+            'password_confirmation' => 'abcde'
+        ]));
+    }
+
+    public function testUnconfirmedPasswordIsInvalid()
+    {
+        $this->assertFalse($this->valid([
+            'password' => '1234567890',
+            'password_confirmation' => ''
+        ]));
+    }
+
+    public function testNonmatchingPasswordConfirmationIsInvalid()
+    {
+        $this->assertFalse($this->valid([
+            'password' => '1234567890',
+            'password_confirmation' => '0987654321'
+        ]));
+    }
+
+    public function testEmptyEmailIsInvalid()
+    {
+        $this->assertFalse($this->valid([
+            'email' => '',
+        ]));
+    }
+
+    public function testNonEmailIsInvalid()
+    {
+        $this->assertFalse($this->valid([
+            'email' => 'mail',
+        ]));
+    }
+
+    public function testLongUtfPasswordIsValid()
+    {
+        $longPass = '☺♘✓"V:%§!$&@€()[]{}\/#*+~FrK;?s6F:=-L\BG$L\w*96l-c-o^$2R\'~rDzo#diWSR=l5*59~WoVfS/gq1aLTykRGP-J3iw;07}ate*Nd>gvnj$[EEDepDwz;mVud4/OL7g\'*)xM.E+5RvWzHH4';
+
+        $this->assertTrue($this->valid([
+            'password' => $longPass,
+            'password_confirmation' => $longPass,
+        ]));
+    }
+       
 }
--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -20,4 +20,21 @@ class UnitTest extends \Tests\TestCase {
         $this->assertEquals('test@example.com', $validUser->email);
         $this->assertTrue(app('hash')->check('testpass', $validUser->password));
     }
+
+    public function testLongUtfPasswordWorks()
+    {
+        // Bcrypt won't actually use the whole string when hashing, but make
+        // sure the system accepts really long passwords.
+        $longPass = '☺♘✓"V:%§!$&@€()[]{}\/#*+~FrK;?s6F:=-L\BG$L\w*96l-c-o^$2R\'~rDzo#diWSR=l5*59~WoVfS/gq1aLTykRGP-J3iw;07}ate*Nd>gvnj$[EEDepDwz;mVud4/OL7g\'*)xM.E+5RvWzHH4';
+
+        $validAttribs = array(
+            'username' => 'TestUser',
+            'email' => 'test@example.com',
+            'password' => bcrypt($longPass),
+            'remember_token' => 'REMEMBERME',
+        );
+        $validUser = new User($validAttribs);
+
+        $this->assertTrue(app('hash')->check($longPass, $validUser->password));
+    }
 }
