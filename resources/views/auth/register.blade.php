5af134d6ba940022ef6db9c48cf90def2fa755fe
Adam Goforth <code@adamgoforth.com>
2016-01-01 10:18:04 -0500

    Display inline form errors on the register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,26 +12,57 @@
     <div class="row">
 
         <div class="col-sm-6 col-sm-offset-3 col-md-4 col-md-offset-4">
-            {!! Form::open(array('url' => '/register', 'class' => 'user-form')) !!}
-            {!! Form::token() !!}
+            <form class="user-form" role="form" method="POST" action="{{ url('/register') }}">
+                {!! csrf_field() !!}
 
-            {!! Form::label('username', 'Username') !!}
-            {!! Form::text('username', '', array('class' => 'input-block-level')) !!}
+                <div class="form-group{{ $errors->has('username') ? ' has-error' : '' }}">
+                    <label class="control-label">Username</label>
+                    <input type="text" class="form-control" name="username" value="{{ old('username') }}">
 
-            {!! Form::label('email', 'Email Address') !!}
-            {!! Form::text('email', '', array('class' => 'input-block-level')) !!}
+                    @if ($errors->has('username'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('username') }}</strong>
+                        </span>
+                    @endif
+                </div>
 
-            {!! Form::label('password', 'Password') !!}
-            {!! Form::password('password', array('class' => 'input-block-level')) !!}
+                <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                    <label class="control-label">E-Mail Address</label>
+                    <input type="email" class="form-control" name="email" value="{{ old('email') }}">
 
-            {!! Form::label('password_confirmation', 'Confirm Password') !!}
-            {!! Form::password('password_confirmation', array('class' => 'input-block-level')) !!}
+                    @if ($errors->has('email'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('email') }}</strong>
+                        </span>
+                    @endif
+                </div>
 
-            {!! Form::submit('Create Account', array('class' => 'btn btn-primary btn-lg')) !!}
+                <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                    <label class="control-label">Password</label>
+                    <input type="password" class="form-control" name="password" value="{{ old('password') }}">
+                    
+                    @if ($errors->has('password'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('password') }}</strong>
+                        </span>
+                    @endif
+                </div>
 
-            <div class="privacy-message">We will never sell or give away your email address.</div>
+                <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
+                    <label class="control-label">Confirm Password</label>
+                    <input type="password" class="form-control" name="password_confirmation" value="{{ old('password_confirmation') }}">
+                    
+                    @if ($errors->has('password_confirmation'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('password_confirmation') }}</strong>
+                        </span>
+                    @endif
+                </div>
 
-            {!! Form::close() !!}
+                <button type="submit" class="btn btn-primary btn-lg">Create Account</button>
+
+                <div class="privacy-message">We will never sell or give away your email address.</div>
+            </form>
         </div>
 
     </div>
