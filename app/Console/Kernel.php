9ed3d574d9c6b8b575376b9ae589eeefb01179dd
Adam Goforth <code@adamgoforth.com>
2016-01-18 21:52:21 +0100

    Add artisan deploy command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         Commands\Inspire::class,
+        Commands\Deploy::class,
     ];
 
     /**
@@ -24,7 +25,5 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('inspire')
-                 ->hourly();
     }
 }
