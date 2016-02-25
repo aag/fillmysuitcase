56ceffa9c8120d3c59f81b9bf1d5031b2093da8f
Adam Goforth <code@adamgoforth.com>
2016-02-25 20:19:03 +0100

    Upgrade AngularJS to 1.5.0

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -89,8 +89,8 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js"></script>
 
-        <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.9/angular.min.js"></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.9/angular-resource.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.0/angular.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.0/angular-resource.min.js"></script>
     @else
         <script src="/js/libs/jquery-2.1.4.min.js"></script>
         <script src="/js/libs/underscore.min.js"></script>
