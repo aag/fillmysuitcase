928b9288a9c5e9aadff38460fd50ab7aaa30ff0e
Adam Goforth <code@adamgoforth.com>
2026-09-19 14:32:40 -0400

    Update version number of PHP FPM for deploy

--- deploy.php
+++ deploy.php
@@ -73,7 +73,7 @@ task('deploy:mix', function() {
 })->desc('Execute Laravel Mix');
 
 task('deploy:fpm_restart', function() {
-    $fpmServiceName = trim(run("ls -1 /var/run/php | grep 'php8\.3-fpm\.sock' | sed 's/\.sock//'"));
+    $fpmServiceName = trim(run("ls -1 /var/run/php | grep 'php8\.4-fpm\.sock' | sed 's/\.sock//'"));
     $output = run("sudo service {$fpmServiceName} restart");
     //writeln('<info>'.$output.'</info>');
 })->desc('Restart PHP-FPM');
